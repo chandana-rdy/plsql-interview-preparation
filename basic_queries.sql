783f1d24ec21079ed781b0b1c370c7f2e1421569
chandana-rdy <ece.182g1a0459@gmail.com>
2026-03-04 22:39:22 +0530

    Create basic_queries.sql

---
+++ basic_queries.sql
@@ -0,0 +1,6 @@
+-------select all records from table
+select * from employees;
+--------employees with salary > 50000
+select * from employees where salary > 50000;
+-----------order by salary
+select * from employees order by salary desc;
